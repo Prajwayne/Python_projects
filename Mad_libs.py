1d71c6aa638cf4780aafbb6dd5574430d6faef12
Prajwal Kulkarni <praju.9741@gmail.com>
2024-05-15 15:42:19 +0100

    madlibs

---
+++ Mad_libs.py
@@ -0,0 +1,13 @@
+#String Concatination 
+# Take a String 
+str = "Hello"
+print("Programmer {}".format(str))
+#Basically we can add the string in the {} braces 
+#the cleanest way 
+#Basic
+adj = input("Adjective")
+verb1 =input("Verb:")
+verb2 = input("Verb:")
+famous_person = input("Programmer")
+madlib = f"Programming is so {adj}! But It makes me exited all the time because I love to {verb1}. It is important to be {verb2}. One of the best programmer is {famous_person}"
+print(madlib)
