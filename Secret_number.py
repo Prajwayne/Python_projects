a9dc63322dd411c8381c5acb251c507aa59b0407
Prajwal Kulkarni <praju.9741@gmail.com>
2024-05-17 12:33:30 +0100

    Computer guesses the correct number

--- Secret_number.py
+++ Secret_number.py
@@ -15,3 +15,27 @@ def guess(x):
 
 x = int(input("Whats the maximum of random numbers can you guess"))
 guess(x)
+#This function enables computer to guess the secret number 
+
+def computer_guess(x):
+    #set low and high.(Basically computer has to guess a number in the range provided)
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+        #Anything below low: stop considering || anything above high stop considering
+         guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too hight (H), too low (L), or correct (C)??').lower()
+        #if the guess is too high: adjust the upper bound
+        if feedback =='h':
+            high = guess - 1
+        elif feedback =='l':
+            low = guess + 1
+    print(f"Absolute Genius! Thats is right,{guess}")
+
+computer_guess(10)
+
+
